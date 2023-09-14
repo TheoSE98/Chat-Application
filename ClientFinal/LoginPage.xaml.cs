bb029f3ed7e3013d948f065ac615d68a7b1dfc2b
Theo Hancock <theo.hancock98@gmail.com>
2023-09-14 18:33:59 +0800

    Fixed the GUI

---
+++ ClientFinal/LoginPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClientFinal
+{
+    /// <summary>
+    /// Interaction logic for LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_LogIn(object sender, RoutedEventArgs e)
+        {
+            String username;
+            username = usernameEntryBox.Text;
+
+            /*            NavigationContext navigationContext;
+                        string id = navigationContext.Parameters["ID"];
+            */
+
+            this.NavigationService.Navigate(new Uri("HomePage.xaml", UriKind.Relative));
+        }
+    }
+}
